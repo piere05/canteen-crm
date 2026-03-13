f31867d6ea3ce25511af9f274b73d8343d5cbf36
piere05 <piereshc@gmail.com>
2026-03-13 16:37:52 +0530

    ds

--- canteen_crm/settings.py
+++ canteen_crm/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from tracemalloc import StatisticDiff
-import os 
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-3ei=e6*flk=@&3izrw=#@=7(xfet1!dh!pd0=$9!9vl70lp^c^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,14 +77,9 @@ WSGI_APPLICATION = 'canteen_crm.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_canteen_crm',
-        'USER' : 'postgres',
-        'PASSWORD' : '1234',
-        'HOST': 'localhost',
-        'PORT' : '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:1234@localhost:5432/db_canteen_crm'
+    )
 }
 
 
